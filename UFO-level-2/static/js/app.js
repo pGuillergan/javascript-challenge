44027cbcc1954da14b9cebe9d424498f12679d56
Chinchinberry <jonjon0424@gmail.com>
2019-11-19 22:40:41 -0800

    all done

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -7,22 +7,23 @@ var form = d3.select("form");
 //var table = d3.select("table");
 var tableBody = d3.select("tbody");
 var button = d3.select("#button");
+var selected = "datetime";
 
 function handleChange(event){
-	var input_date = d3.event.target.value;
+	var input_data = d3.event.target.value;
 	// console.log(input_date);
 	tableBody.selectAll("tr").remove();
-	loadTable(input_date);
+	loadTable(input_data);
 };
 
 input_text.on("change", handleChange);
 
 button.on("click", handleChange);
 
-function loadTable(input_date){
+function loadTable(input_data){
 	tableData.forEach(function(entry){
 		//console.log(entry["shape"]);
-		if (entry["datetime"] === input_date){
+		if (entry[selected] === input_data){
 			//console.log(entry);
 			var tableRow = tableBody.append("tr");
 
@@ -38,17 +39,24 @@ function loadTable(input_date){
 function getChoice(choice){
 
 	switch(choice){
-		case "date":
+		case "datetime":
+			d3.select("input").attr("placeholder", "1/11/2011")
 			break;
 		case "city":
+			d3.select("input").attr("placeholder", "ex. bonita")
 			break;
 		case "state":
+			d3.select("input").attr("placeholder", "ex. ca")
 			break;
 		case "country":
+			d3.select("input").attr("placeholder", "ex. us")
 			break;
 		case "shape":
+			d3.select("input").attr("placeholder", "ex. triangle")
 			break;
-		case "duration":
+		case "durationMinutes":
+			d3.select("input").attr("placeholder", "ex. 1 min")
 			break;
 	};
+	selected = choice;
 };
