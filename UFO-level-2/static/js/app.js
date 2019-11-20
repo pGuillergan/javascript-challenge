f4dba21034ac12bbefe164d552b22f2c0c642551
Paul G <38454806+pGuillergan@users.noreply.github.com>
2019-11-19 16:57:32 -0800

    some updeees

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,3 +2,53 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var input_text = d3.select("#datetime");
+var form = d3.select("form");
+//var table = d3.select("table");
+var tableBody = d3.select("tbody");
+var button = d3.select("#button");
+
+function handleChange(event){
+	var input_date = d3.event.target.value;
+	// console.log(input_date);
+	tableBody.selectAll("tr").remove();
+	loadTable(input_date);
+};
+
+input_text.on("change", handleChange);
+
+button.on("click", handleChange);
+
+function loadTable(input_date){
+	tableData.forEach(function(entry){
+		//console.log(entry["shape"]);
+		if (entry["datetime"] === input_date){
+			//console.log(entry);
+			var tableRow = tableBody.append("tr");
+
+			Object.entries(entry).forEach(function([key, value]) {
+    			//console.log(key, value);
+    			tableRow.append("td").text(value);
+  			});	
+		};
+		
+	});
+};
+
+function getChoice(choice){
+
+	switch(choice){
+		case "date":
+			break;
+		case "city":
+			break;
+		case "state":
+			break;
+		case "country":
+			break;
+		case "shape":
+			break;
+		case "duration":
+			break;
+	};
+};
